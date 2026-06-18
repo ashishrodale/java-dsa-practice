67b428861f39f9ee95defa9e45975344f7f98398
Ashish Rodale <ashishrodale18@gmail.com>
2026-06-18 14:57:18 +0530

    Add DataTypes class to demonstrate Java data types
    Demonstrates various data types in Java with examples.

---
+++ Java-Basics/DataTypes.java
@@ -0,0 +1,32 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        // Integer types
+        byte age = 20;
+        short year = 2025;
+        int population = 1000000;
+        long distance = 9876543210L;
+
+        // Decimal types
+        float price = 99.99f;
+        double pi = 3.14159265359;
+
+        // Character type
+        char grade = 'A';
+
+        // Boolean type
+        boolean isJavaFun = true;
+
+        // Display values
+        System.out.println("Byte: " + age);
+        System.out.println("Short: " + year);
+        System.out.println("Int: " + population);
+        System.out.println("Long: " + distance);
+
+        System.out.println("Float: " + price);
+        System.out.println("Double: " + pi);
+
+        System.out.println("Char: " + grade);
+        System.out.println("Boolean: " + isJavaFun);
+    }
+}
