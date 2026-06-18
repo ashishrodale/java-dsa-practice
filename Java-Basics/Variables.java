a362fe66372b1b48f636e5682b7704ed4df6c92d
Ashish Rodale <ashishrodale18@gmail.com>
2026-06-18 14:49:14 +0530

    Add Variables class with main method

---
+++ Java-Basics/Variables.java
@@ -0,0 +1,8 @@
+public class Variables{
+  public static void main(String[] args){
+    int age = 18;
+    String name = "Ashish";
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+  }
+}
